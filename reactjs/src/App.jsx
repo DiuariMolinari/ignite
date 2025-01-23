1e534172e9a5c93ceb989089ea0ea3174133ed6b
Diuari Molinari <diuari.molinari@gmail.com>
2025-01-22 22:07:22 -0300

    Componente Header da aplicação.

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
+
+import './global.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello world</h1>
+      <Header />
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ipsum magnam facilis delectus inventore. Architecto aliquid obcaecati doloribus quaerat, nobis aut accusantium voluptatibus suscipit. Necessitatibus vel quas quam deserunt qui.</h1>
     </>
   )
 }
